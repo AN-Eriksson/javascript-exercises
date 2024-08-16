97071ffdff5d7c1a75f3e7ba6f42f3963e3d6899
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-16 13:27:45 +0200

    Complete javascript exercises 1-2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(words, times) {
+
+    if (times < 0) {
+        return "ERROR"
+    }
+    let repeatedString = "";
+    
+    for (let i = 1; i <= times; i++) {
+
+        repeatedString += words;    
+    }
+    return repeatedString;
 
 };
 
