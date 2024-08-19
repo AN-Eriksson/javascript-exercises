cb86dbfab48480dc5fa8982cca02990d98a37b6a
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-19 12:30:45 +0200

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let newStr = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str.charAt(i);        
+    }
+
+    return newStr;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
