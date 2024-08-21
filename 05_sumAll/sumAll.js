9a35d93c17e9380702244c0cfe3b6f4e316e9780
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-21 19:48:17 +0200

    Complete "05_sumAll"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let smallest = 0;
+    let largest = 0;
+    let totalSum = 0;
+
+    /* Return ERROR if any number is less than 0 or is not an integer. */
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    /* Find the smallest number for the loop */
+
+    if (num1 > num2) {
+        smallest = num2;
+        largest = num1;
+    } else {
+        smallest = num1;
+        largest = num2;
+    }
+
+    /* Loop from smallest to largest in one integer steps, summing to totalSum */
+
+    for (let i = smallest; i <= largest; i++ ){
+        totalSum += i;
+    }
+
+    return totalSum;
 
 };
 
