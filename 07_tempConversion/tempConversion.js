c3e48c81d48a296402d4bae726b162a988ff292f
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-21 20:25:14 +0200

    Complete "07_tempConversion"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inFahrenheit) {
+  return Math.round((inFahrenheit - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inCelsius) {
+  return Math.round((inCelsius * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
