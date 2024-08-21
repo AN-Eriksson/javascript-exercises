414d3adb0d5f4cc2ebb0747077f40aaac0e669ca
Andreas Eriksson <100693629+AN-Eriksson@users.noreply.github.com>
2024-08-21 12:27:37 +0200

    Complete "removeFromArray"

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toBeRemoved) {  
+
+
+    /* loopa genom toBeRemoved och för varje värde:
+        sök i array efter korrekt index och splajsa det. */
+
+    let index = 0;
+    for (let element of toBeRemoved) {      
+        while (array.includes(element)) {
+            index = array.indexOf(element);
+            array.splice(index, 1);
+        }           
+    }
+    return array; 
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+
+
+
